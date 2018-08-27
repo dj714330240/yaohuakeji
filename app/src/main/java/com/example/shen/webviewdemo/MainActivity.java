00fd858abc109ab1d48eca89eff651e028d67b54
714330240@qq.com <714330240@qq.com>
2018-08-27 23:14:10 +0800

    耀华科技

--- app/src/main/java/com/example/shen/webviewdemo/MainActivity.java
+++ app/src/main/java/com/example/shen/webviewdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.shen.webviewdemo;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -21,10 +24,70 @@ public class MainActivity extends Activity {
     private void initView() {
         String url = "https://www.yaohuakj.cn/";
         webView = (WebView) findViewById(R.id.webview);
+        //支持javascript
+        webView.getSettings().setJavaScriptEnabled(true);
+        // 设置可以支持缩放
+        webView.getSettings().setSupportZoom(true);
+        // 设置出现缩放工具
+        webView.getSettings().setBuiltInZoomControls(true);
+        //扩大比例的缩放
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBlockNetworkImage(false);//解决图片不显示
+        //自适应屏幕
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.getSettings().setLoadWithOverviewMode(true);
         //启用支持JavaScript
         webView.getSettings().setJavaScriptEnabled(true);
         //启用支持DOM Storage
         webView.getSettings().setDomStorageEnabled(true);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setLoadWithOverviewMode(true);
+/**webView.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);这个属性
+ 可以让webview只显示一列，也就是自适应页面大小 不能左右滑动，但在使用中发现，只针对4.4以下有效，因为4.4的webview内核改了，Google也在api中说了，要么改html样式，要么改变WebView；
+ */
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP) {
+
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING);
+        } else {
+            webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        }
+/**
+ * 用WebView显示图片，可使用这个参数 设置网页布局类型： 1、LayoutAlgorithm.NARROW_COLUMNS ：
+ * 适应内容大小 2、LayoutAlgorithm.SINGLE_COLUMN:适应屏幕，内容将自动缩放
+ *   webSettings.setLayoutAlgorithm(LayoutAlgorithm.NARROW_COLUMNS);
+ */
+
+
+//下方代码已经过时（不再沿用）
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int mDensity = metrics.densityDpi;
+        if (mDensity == 240) {
+            webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        } else if (mDensity == 160) {
+            webSettings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+        } else if(mDensity == 120) {
+            webSettings.setDefaultZoom(WebSettings.ZoomDensity.CLOSE);
+        }else if(mDensity == DisplayMetrics.DENSITY_XHIGH){
+            webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        }else if (mDensity == DisplayMetrics.DENSITY_TV){
+            webSettings.setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        }else{
+            webSettings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+        }
+
         //加载web资源
         webView.loadUrl(url);
         //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
